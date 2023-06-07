efc05f37aad6cfd4bd76591a2a668b56e6029472
DongYoung90 <135804072+DongYoung90@users.noreply.github.com>
2023-06-08 00:30:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
-import time
-import random
+
 
 
 # ------------------------Frame------------------------ #
